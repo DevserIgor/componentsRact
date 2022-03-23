e3df3f679198158b05fb7d6727d7834544d628db
Igor dev <devserigor@gmail.com>
2022-03-23 18:07:18 -0300

    upload with sdk javascript v3

--- src/pages/Upload/index.js
+++ src/pages/Upload/index.js
@@ -1,28 +1,31 @@
 import { Box, CssBaseline } from '@mui/material';
 import React, { useState } from 'react';
-import AWS from "aws-sdk";
 import { Container } from './styles';
+import { PutObjectCommand } from "@aws-sdk/client-s3";
+import { S3Client } from "@aws-sdk/client-s3";
 
 // const S3_BUCKET = "ppgbuckettest";
 const S3_BUCKET = "testeigao";
 const REGION = "sa-east-1";
 
-AWS.config.update({
-  accessKeyId: "AKIAZAAQ4ILBZPXCO5YU",
-  secretAccessKey: "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",
-});
-
-const myBucket = new AWS.S3({
-  params: { Bucket: S3_BUCKET },
+const config = {
   region: REGION,
-});
+  apiVersion: "latest",
+  params: { Bucket: S3_BUCKET },
+  credentials: {
+    accessKeyId: "AKIAZAAQ4ILBZPXCO5YU",
+    secretAccessKey: "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",
+  },
+};
+
+const client = new S3Client(config);
 
 
 function Upload() {
   const [progress, setProgress] = useState(0);
   const [selectedFile, setSelectedFile] = useState(null);
 
-  const uploadFile = (file) => {
+  const uploadFile = async (file) => {
     const params = {
       ACL: "public-read",
       Body: file,
@@ -30,14 +33,18 @@ function Upload() {
       Key: file.name,
     };
 
-    myBucket
-      .putObject(params)
-      .on("httpUploadProgress", (evt) => {
-        setProgress(Math.round((evt.loaded / evt.total) * 100));
-      })
-      .send((err) => {
-        if (err) console.log(err);
-      });
+    const data = await client
+      .send(new PutObjectCommand(params));
+      // .on("httpUploadProgress", (evt) => {
+      //   setProgress(Math.round((evt.loaded / evt.total) * 100));
+      // })
+      // .send((err) => {
+      //   if (err) console.log(err);
+      // });
+    
+    console.log(data);
+
+    
   };
 
 
